bde9a34597f3433ae3db820125a580806dfa70e5
npodewitz <nils.podewitz@googlemail.com>
2019-02-17 19:12:28 +0100

    Migration does now work with own additions. Probably as to be recreated when upstream changes model (./manage.py makemigrations --merge)

--- src/documents/migrations/9001_folder_file_number.py
+++ src/documents/migrations/9001_folder_file_number.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('documents', '0022_auto_20181007_1420'),
+        ('documents', '0001_initial'),
     ]
 
     operations = [
